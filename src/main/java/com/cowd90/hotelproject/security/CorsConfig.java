37d970a99518cdcab7740a920ea21af3d5494863
cowd90 <tienn.dungg.980@gmail.com>
2024-02-29 18:23:24 +0700

    add access control allow origin

--- src/main/java/com/cowd90/hotelproject/security/CorsConfig.java
+++ src/main/java/com/cowd90/hotelproject/security/CorsConfig.java
@@ -7,15 +7,24 @@ import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
 @Configuration
 @EnableWebMvc
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
     private static final Long MAX_AGE = 3600L;
     private static final int CORS_FILTER_ORDER = -102;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods(HttpMethod.GET.name());
+    }
 
     @Bean
     public CorsFilter corsFilter() {
